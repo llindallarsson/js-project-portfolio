f85d6a1033fb468a3dcd38577a918472c84870fb
llindallarsson <larsson.ll@hotmail.com>
2025-04-23 15:39:23 +0200

    bug with image fixed

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,14 +1,19 @@
+import styled from "styled-components";
 import { SectionHeader } from "../components/Typography";
 import ProjectCard from "../components/ProjectCard";
 
+const ProjectSectionWrapper = styled.section`
+  margin: 120px 0;
+`;
+
 const ProjectsSection = () => {
   return (
-    <>
+    <ProjectSectionWrapper>
       <SectionHeader>Featured projects</SectionHeader>
       <ProjectCard
         title="Business project"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        imageSrc="src/img/business_project.png"
+        imageSrc="/images/business_project.png"
         imageAlt="Business project"
         tech={["Hej", "Ja"]}
         liveLink="https://github.com/llindallarsson"
@@ -17,7 +22,7 @@ const ProjectsSection = () => {
       <ProjectCard
         title="Recipe project"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        imageSrc="src/img/recipe_project.png"
+        imageSrc="/images/recipe_project.png"
         imageAlt="Recipe project"
         tech={["Hej", "Ja"]}
         liveLink="https://github.com/llindallarsson"
@@ -26,13 +31,13 @@ const ProjectsSection = () => {
       <ProjectCard
         title="Accessibility project"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        imageSrc="src/img/accessibility_project.png"
+        imageSrc="/images/accessibility_project.png"
         imageAlt="Accessibility project"
         tech={["Hej", "Ja"]}
         liveLink="https://github.com/llindallarsson"
         codeLink="https://github.com/llindallarsson"
       ></ProjectCard>
-    </>
+    </ProjectSectionWrapper>
   );
 };
 
